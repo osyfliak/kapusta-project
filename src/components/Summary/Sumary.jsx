e8ea405d32a0fa02c155de493c46eab844d3a210
Osyfliak Ivan <osuflak22@gmail.com>
2023-02-27 15:02:15 +0200

    add components and api

---
+++ src/components/Summary/Sumary.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Sumary = () => {
+  return (
+    <div>Sumary</div>
+  )
+}
---
+++ src/services/kapusta-api.js
@@ -0,0 +1,87 @@
+import { privateApi, publicApi } from 'http/http';
+
+export const toket = {
+  set: token => {
+    privateApi.defaults.headers.Authorization = `Bearer ${token}`;
+  },
+  unSet: () => {
+    privateApi.defaults.headers.Authorization = '';
+  },
+};
+
+export const login = async () => {
+  const { data } = await publicApi.post('/auth/login');
+  return data;
+};
+
+export const register = async () => {
+  const { data } = await publicApi.post('/auth/register');
+  return data;
+};
+
+export const logout = async () => {
+  const { data } = await privateApi.post('/auth/logout');
+  return data;
+};
+
+export const refresh = async () => {
+  const { data } = await privateApi.post('/auth/refresh');
+  return data;
+};
+
+// request:
+//     "description": "Income description",
+//     "amount": 100,
+//     "date": "2020-12-31"
+//
+
+export const addTransactionIncome = async request => {
+  const { data } = await privateApi.post('/transaction/income', request);
+  return data;
+};
+
+export const addTransactionExpense = async request => {
+  const { data } = await privateApi.post('/transaction/expense', request);
+  return data;
+};
+
+export const getTransactionIncome = async () => {
+  const { data } = await privateApi.get('/transaction/income');
+  return data;
+};
+
+export const getTransactionExpense = async () => {
+  const { data } = await privateApi.get('/transaction/expense');
+  return data;
+};
+
+export const deleteTransaction = async id => {
+  const { data } = await privateApi.get('/transaction/expense', id);
+  return data;
+};
+
+export const getTransactionIncomeCategories = async () => {
+    const { data } = await privateApi.get('/transaction/income-categories');
+    return data;
+  };
+
+  export const getTransactionExpenseCategories = async () => {
+    const { data } = await privateApi.get('/transaction/expense-categories');
+    return data;
+  };
+
+  export const getTransactionPeriodData = async (date) => {
+    const { data } = await privateApi.get('/transaction/period-data', date);
+    return data;
+  };
+
+  export const updateBalance= async (request) => {
+    const { data } = await privateApi.get('/user/balance',request );
+    return data;
+  };
+
+  export const getUser = async (date) => {
+    const { data } = await privateApi.get('/user', );
+    return data;
+  };
+
