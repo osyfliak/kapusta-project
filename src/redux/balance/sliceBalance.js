d28a1d61cda64ac64f5e3f6908bc71b245ec1c71
Osyfliak Ivan <osuflak22@gmail.com>
2023-03-01 19:12:58 +0200

    Update sliceBalance.js

--- src/redux/balance/sliceBalance.js
+++ src/redux/balance/sliceBalance.js
@@ -1,14 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { logIn, refreshUser } from 'redux/auth/operation';
 import { setUserBalance } from './operations';
-
 const initialState = {
   balance: 0,
   currentPeriod: { month: '', year: '' },
   isLoadinng: false,
   error: null,
 };
-
 const balanceSlice = createSlice({
   name: 'balance',
   initialState,
@@ -19,18 +17,8 @@ const balanceSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(logIn.pending, state => {
-        state.isLoadinng = true;
-        state.error = null;
-      })
       .addCase(logIn.fulfilled, (state, { payload }) => {
-        state.isLoadinng = false;
-        state.error = null;
         state.balance = payload.userData.balance;
-      })
-      .addCase(logIn.rejected, state => {
-        state.isLoadinng = false;
-        state.error = true;
       })
       .addCase(setUserBalance.pending, state => {
         state.isLoadinng = true;
@@ -39,18 +27,16 @@ const balanceSlice = createSlice({
       .addCase(setUserBalance.fulfilled, (state, { payload }) => {
         state.error = null;
         state.isLoadinng = false;
-        console.log(payload);
-        state.balance = payload;
+        state.balance = payload.newBalance;
       })
       .addCase(setUserBalance.rejected, (state, { payload }) => {
         state.isLoadinng = false;
         state.error = payload;
       })
       .addCase(refreshUser.fulfilled, (state, { payload }) => {
-        state.balance = payload.balance ?? 0;
+        state.balance = payload.user.balance ?? 0;
       });
   },
 });
-
 export default balanceSlice.reducer;
 export const { setCurrentPeriod } = balanceSlice.actions;
