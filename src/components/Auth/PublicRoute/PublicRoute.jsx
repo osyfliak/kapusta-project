98c0b799b0d29b28abcb1ee09a770bad92776b8f
Osyfliak Ivan <osuflak22@gmail.com>
2023-03-01 15:10:54 +0200

    Update PublicRoute.jsx

--- src/components/Auth/PublicRoute/PublicRoute.jsx
+++ src/components/Auth/PublicRoute/PublicRoute.jsx
@@ -4,5 +4,5 @@ import { selectToken } from 'redux/selector';
 
 export const PublicRoute = () => {
   const token = useSelector(selectToken);
-  return token ? <Navigate to="/home/expenses" /> : <Outlet />;
+  return token ? <Navigate to="/home" /> : <Outlet />;
 };
