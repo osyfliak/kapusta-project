13e0a6f0cdc34f244c16b127598e864c244d7f5d
Rostyslav Kachor <rostynia@gmail.com>
2023-03-01 17:57:56 +0200

    add modal logout

--- src/components/ModalLogOut/ModalLogOut.jsx
+++ src/components/ModalLogOut/ModalLogOut.jsx
@@ -1,42 +1,90 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
 import {
   Backdrop,
-  Button,
-  ButtonDiv,
-  CloseBtn,
-  Confirm,
-  Content,
-  Modal,
+  ModalContent,
   Title,
+  ButtonsBox,
+  CloseButton,
+  Modal,
 } from './Modal.styled';
-import { GrClose } from 'react-icons/gr';
+import close from '../../images/close.svg';
+import { ModalButtonOrange } from './ModalButtonOrange';
+import { ModalButtonWhite } from './ModalButtonWhite';
 
-export const ModalLogOut = ({ title }) => {
-  const [modal, setModal] = useState();
 
-  const toggleModal = () => {
-    setModal(!modal);
+const modalRoot = document.getElementById('modal-root');
+const body = document.querySelector('body');
+
+
+export const ModalLogOut = ({
+  children,
+  closeModal,
+  dispatch,
+  changeBalance,
+  text,
+}) => {
+ 
+
+ 
+  const handleBackdropClose = event => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
   };
+ 
+  useEffect(() => {
+    const handleEscapeClose = event => {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    };
 
-  return (
-    <>
-      <Confirm onClick={toggleModal}>Exit</Confirm>
-      {modal && (
-        <Modal>
-          <Backdrop onClick={toggleModal}>
-            <Content>
-              <Title>{title}</Title>
-              <ButtonDiv>
-                <Button>YES</Button>
-                <Button>NO</Button>
-              </ButtonDiv>
-              <CloseBtn onClick={toggleModal}>
-                <GrClose size={18} />
-              </CloseBtn>
-            </Content>
-          </Backdrop>
-        </Modal>
-      )}
-    </>
+    window.addEventListener('keydown', handleEscapeClose);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscapeClose);
+      body.classList.toggle('no-scroll');
+    };
+  },[closeModal]);
+
+  return createPortal(
+    // Backdrop
+    <Backdrop
+      className="modal-backdrop"
+      onClick={handleBackdropClose}
+    >
+      
+      <Modal>
+       
+        <CloseButton onClick={closeModal}>
+          <img src={close} alt="close" />
+        </CloseButton>
+        <ModalContent>
+          <Title>{children}</Title>
+          <ButtonsBox>
+            <ModalButtonOrange
+              dispatch={dispatch}
+              closeModal={closeModal}
+              changeBalance={changeBalance}
+            >
+              {text ? text : 'YES'}
+            </ModalButtonOrange>
+            <ModalButtonWhite closeModal={closeModal}>NO</ModalButtonWhite>
+          </ButtonsBox>
+        </ModalContent>
+      </Modal>
+    </Backdrop>,
+    modalRoot
   );
 };
+
+ModalLogOut.propTypes = {
+  children: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  changeBalance: PropTypes.func,
+  text: PropTypes.string,
+};
+
