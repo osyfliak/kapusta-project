623c5df832910d5d6b006a9e6b68624921d34905
Osyfliak Ivan <osuflak22@gmail.com>
2023-03-01 18:12:26 +0200

    1

--- src/redux/balance/operations.js
+++ src/redux/balance/operations.js
@@ -1,12 +1,15 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import {  updateBalance } from 'services/kapusta-api';
 
+
 export const setUserBalance = createAsyncThunk(
   'balance/setBalance',
 
   async (request, { rejectWithValue, getState }) => {
+
     try {
       const resp = await updateBalance(request);
+      
       console.log(resp);
       return resp;
     } catch ({ response }) {
