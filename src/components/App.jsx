84f9082fde03b3fffad1fe176a069e8e534b4ea0
Viktoriia181818 <vcibulskaa4@gmail.com>
2023-03-01 09:52:57 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
+import { Auth } from 'pages/Auth/Auth';
 import LoginPage from 'pages/Auth/LoginPage/LoginPage';
 import RegisterPage from 'pages/Auth/RegisterPage/RegisterPage';
 import ThereIsNoSuchPage from 'pages/Auth/ThereIsNoSuchPage/ThereIsNoSuchPage';
+import { Home } from 'pages/Home/Home';
+import { Report } from 'pages/Report/Report';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
@@ -64,7 +67,11 @@ export const App = () => {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="*" element={<Navigate to="/login" />} />
-              </Route>
+            </Route>
+               <Route path="/home" element={<Navigate to="/home" />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/report" element={<Report />} />
+              <Route path="/auch" element={<Auth />} />
               <Route path="*" element={<ThereIsNoSuchPage />} />
             </Route>
         </Routes>
